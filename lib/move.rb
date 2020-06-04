11e9189464c2cb43daa659fec23906cbde89465f
matt joseloff <msjo425@gmail.com>
2020-06-04 19:41:36 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(position)
-  position = position.to_i
+  position = -1 + position.to_i
 end
 
 def move(array,position,value = " X ")
